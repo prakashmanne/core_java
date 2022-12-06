9743bd475e36b4ccfe1381c078e29e490bcbe26f
javawithlwl <javawithlwl@gmail.com>
2022-12-06 07:22:11 +0530

    Added arrays examples

---
+++ src/main/java/com/careerit/cj/basics/arrays/ArrayExample1.java
@@ -0,0 +1,10 @@
+package com.careerit.cj.basics.arrays;
+
+public class ArrayExample1 {
+  public static void main(String[] args) {
+    int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+}
