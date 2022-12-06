c349dbe46720ff97e0410600e5b22e792310c740
javawithlwl <119342072+javawithlwl@users.noreply.github.com>
2022-12-06 07:27:18 +0530

    Added extra log message to show array elements

--- src/main/java/com/careerit/cj/basics/arrays/ArrayExample1.java
+++ src/main/java/com/careerit/cj/basics/arrays/ArrayExample1.java
@@ -1,4 +1,5 @@
 package com.careerit.cj.basics.arrays;
+import java.util.*;
 
 public class ArrayExample1 {
   public static void main(String[] args) {
@@ -6,5 +7,6 @@ public class ArrayExample1 {
     for (int i = 0; i < arr.length; i++) {
       System.out.println(arr[i]);
     }
+    System.out.println(Arrays.toString(arr));
   }
 }
