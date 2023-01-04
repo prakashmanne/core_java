2780dbefa20a91907788764af0612ab7d9005dd5
prakashmanne <manneprakash0@gmail.com>
2023-01-04 16:45:36 -0600

    new project

---
+++ iplstat/src/main/java/com/careerit/iplstat/array/array.java
@@ -0,0 +1,13 @@
+package com.careerit.iplstat.array;
+
+public class array {
+    public static void main (String args[]){
+        int arr[]={1,2,2,3,4,5};
+        for (int i=o; i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+
+    }
+
+}
